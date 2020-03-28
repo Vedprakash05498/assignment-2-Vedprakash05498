2983da73006663af05331f629554c4f4739f33ff
Vedprakash05498 <47717448+Vedprakash05498@users.noreply.github.com>
2020-03-28 19:45:44 +0530

    create a MyPriorityQueue class for problem-3

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,23 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enQueue(new Node(new Student(14, "VED")));
+        queue.enQueue(new Node(new Student(8, "NIKHIL")));
+        queue.enQueue(new Node(new Student(35, "Mohit")));
+        queue.enQueue(new Node(new Student(1, "Shobhit")));
+        queue.enQueue(new Node(new Student(16, "Prince")));
+        queue.printQueue();
+
+
+    }
 }
--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,24 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
+import java.util.ArrayList;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(new Student(1, "Ved", 6, 5));
+        studentList.add(new Student(2, "Ni", 7, 1));
+        studentList.add(new Student(3, "MO", 7, 6));
+        studentList.add(new Student(4, "So", 4, 7));
+        studentList.add(new Student(5, "Pr", 6, 8));
+        MyCircularQueue queue = new MyCircularQueue();
+        queue.enQueue(studentList);
+        queue.printQueue();
+    }
 }
+
